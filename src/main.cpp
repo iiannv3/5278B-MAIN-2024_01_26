1807cd166007e0239ee4f4b127aace7c8510446c
Iiann Baradhwaj <atharvabhardwaj@iCloud.com>
2024-01-26 23:57:36 -0500

    Setting up EZ-Template and 5278B Folder

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,28 @@
 Drive chassis (
   // Left Chassis Ports (negative port will reverse it!)
   //   the first port is the sensored port (when trackers are not used!)
-  {-15, -16}
+  {1,3,5}
 
   // Right Chassis Ports (negative port will reverse it!)
   //   the first port is the sensored port (when trackers are not used!)
-  ,{6, 5}
+  ,{6,8,10}
 
   // IMU Port
-  ,20
+  ,5
 
   // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
   //    (or tracking wheel diameter)
-  ,2.5
+  ,3.25
 
   // Cartridge RPM
   //   (or tick per rotation if using tracking wheels)
-  ,1200
+  ,600
 
   // External Gear Ratio (MUST BE DECIMAL)
   //    (or gear ratio of tracking wheel)
   // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
   // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
-  ,2
+  ,.6
 
 
   // Uncomment if using tracking wheels
