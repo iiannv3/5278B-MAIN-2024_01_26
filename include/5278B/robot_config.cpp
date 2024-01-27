c394d435032c86e09af8ab34ee22ed2c78ca27f0
Nikhil Gajjar <nikgajjar51@gmail.com>
2024-01-27 00:22:04 -0500

    Robot Config

---
+++ include/5278B/robot_config.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "api.h"
+
+extern pros::Motor slapper;
+extern pros::Motor intake;
+extern pros::ADIDigitalOut horizontal_flap_A;
+extern pros::ADIDigitalOut horizontal_flap_B;
+extern pros::ADIDigitalOut vertical_flap_A;
+extern pros::ADIDigitalOut vertical_flap_B;
--- src/5278B/robot_config.cpp
+++ src/5278B/robot_config.cpp
@@ -1,1 +1,13 @@
 #include "main.h"
+#include "pros/adi.hpp"
+#include "pros/motors.h"
+#include "pros/motors.hpp"
+
+pros::Motor slapper(15, pros::E_MOTOR_GEAR_100, false,
+                    pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor intake(15, pros::E_MOTOR_GEAR_600, false,
+                   pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::ADIDigitalOut horizontal_flap_A(3);
+pros::ADIDigitalOut horizontal_flap_B(4);
+pros::ADIDigitalOut vertical_flap_A(1);
+pros::ADIDigitalOut vertical_flap_B(2);
